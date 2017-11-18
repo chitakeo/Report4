a84048f89880299a9e7be52c7855366ad144af5f
Wataru Nakasone <e175729@ie.u-ryukyu.ac.jp>
2017-11-18 13:04:31 +0900

    課題

--- src/jp/ac/uryukyu/ie/e175729/Main.java
+++ src/jp/ac/uryukyu/ie/e175729/Main.java
@@ -2,13 +2,17 @@ package jp.ac.uryukyu.ie.e175729;
 
 public class Main {
     public static void main(String[] args) {
+        {
 
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました");
-            System.err.println(e.getMessage());
+            try {
+                String str = "3．14";
+                System.out.println(str.length());
+                double value;
+                Double.parseDouble(str);
+            } catch (NullPointerException e) {
+                System.out.println("NullPointerExceptionが発生しました");
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
